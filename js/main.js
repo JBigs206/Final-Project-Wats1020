792043d833fbfd1afa5e58e49676c0333bc1750a
jeffreyrichardbigler <jeffreyrichardbigler@gmail.com>
2016-03-12 15:46:32 -0500

    Added elements to HTML, CSS, and JS

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 $( document ).ready(function() {
 	
-$(document).ready(function(){
+/*$(document).ready(function(){
     $(".nav-tabs a").click(function(){
-        $(this).tab('show');
-		
+		$(this).tab('show');
     });
+});	*/
+	$(document).ready(function(){
+		if($('#ResturantsBtn').click())
+   	 	{
+        	$('#ResturantsBtn').tab('show');
+			$('#BarsBtn').tab('hide');
+			$('#StoresBtn').tab('hide');
+			$('#LandmkBtn').tab('hide');
+   		} else if ($('#ResturantsBtn').click())
+			{
+			$('#BarsBtn').tab('show');
+			$('#ResturantsBtn').tab('hide');
+			$('#StoresBtn').tab('hide');
+			$('#LandmkBtn').tab('hide');
+			}
 });	
-
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
@@ -33,68 +46,70 @@ L.control.layers(mapLayers).addTo(map);
 drawLayer.addTo(map);
 
 var marker = L.marker([47.608013, -122.335167]).addTo(map);
+	marker.bindPopup("<b>City Center</b>");
+	
+
+	
 	
 $('#ResturantsBtn').click(function(){
 	var Rspoon = L.marker([47.6081327, -122.33701810000002]).addTo(map);
 	Rspoon.bindPopup("<b>Rusty Spoon</b><br>Gourmet food from Iceland.");
-	});	
+	Rspoon.valueOf()._icon.style.backgroundColor = 'red';
 	
-$('#ResturantsBtn').click(function(){
 	var shark = L.marker([47.61205349999999, -122.3374983]).addTo(map);
 	shark.bindPopup("<b>Shark Fin Biestro</b><br>Chinese Resturant.");
-	});	
+	shark.valueOf()._icon.style.backgroundColor = 'red';
 	
-$('#ResturantsBtn').click(function(){
 	var spagett = L.marker([47.6045172, -122.3306824]).addTo(map);
 	spagett.bindPopup("<b>Spagett</b><br>Authentic Italian food.");
-	});		
+	spagett.valueOf()._icon.style.backgroundColor = 'red';
+	
+	});	
+
 
 $('#BarsBtn').click(function(){
 	var tiny = L.marker([47.6073345, -122.33797850000002]).addTo(map);
 	tiny.bindPopup("<b>The Tiny Elephant</b><br>Hipster Bar.");
-	});
-
-$('#BarsBtn').click(function(){
+	tiny.valueOf()._icon.style.backgroundColor = 'yellow';
+	
 	var peep = L.marker([47.6074048, -122.3428874]).addTo(map);
 	peep.bindPopup("<b>The Peep Hole</b><br>Local dive bar.");
-	});
+	peep.valueOf()._icon.style.backgroundColor = 'yellow';
 	
-$('#BarsBtn').click(function(){
 	var divine = L.marker([47.607118, -122.333257]).addTo(map);
 	divine.bindPopup("<b>Divine</b><br>Popular night club.");
-	});	
+	divine.valueOf()._icon.style.backgroundColor = 'yellow';
+	});
 	
 $('#StoresBtn').click(function(){
 	var jc = L.marker([47.6089511, -122.32684460000002]).addTo(map);
 	jc.bindPopup("<b>JC Quarters</b><br>Clothing store by the people, for the people.");
-	});
+	jc.valueOf()._icon.style.backgroundColor = 'green';
 	
-$('#StoresBtn').click(function(){
 	var bird = L.marker([47.6038212, -122.32417570000001]).addTo(map);
 	bird.bindPopup("<b>Whirly Bird</b><br>Best helicopter store in the country.");
-	});
+	bird.valueOf()._icon.style.backgroundColor = 'green';
 	
-$('#StoresBtn').click(function(){
 	var eye = L.marker([47.60536219999999, -122.32372220000002]).addTo(map);
 	eye.bindPopup("<b>Eye of Mordor</b><br>High end monocle shop.");
-	});	
+	eye.valueOf()._icon.style.backgroundColor = 'green';
+	});
 	
 $('#LandmkBtn').click(function(){
 	var leif = L.marker([47.607624, -122.325608]).addTo(map);
 	leif.bindPopup("<b>Leif Erikson Memorial</b><br>Monument to Leif Erikson.");
-	});
+	leif.valueOf()._icon.style.backgroundColor = 'orange';
 	
-$('#LandmkBtn').click(function(){
 	var jedi = L.marker([47.609012, -122.329924]).addTo(map);
 	jedi.bindPopup("<b>Jedi Temple</b><br>Center of the Jedi Order.");
-	});
+	jedi.valueOf()._icon.style.backgroundColor = 'orange';
 	
-$('#LandmkBtn').click(function(){
 	var pier = L.marker([47.604128, -122.339603]).addTo(map);
 	pier.bindPopup("<b>Fisherman's Pier</b><br>Historical fishing port.");
+	pier.valueOf()._icon.style.backgroundColor = 'orange';
 	});
 	
-	
+
 jQuery.validator.addMethod("lettersonly", function(value, element) 
 	{
 	return this.optional(element) || /^[a-z ]+$/i.test(value);
